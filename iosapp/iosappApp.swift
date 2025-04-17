7e5de2844de9dc49d9352a48986b3c7ab512ddc8
shruthi <sruthi2k6@gmail.com>
2025-04-16 17:29:17 -0700

    Initial Commit

---
+++ iosapp/iosappApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosappApp.swift
+//  iosapp
+//
+//  Created by Shruthi Sathya on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct iosappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosappTests/iosappTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosappTests.swift
+//  iosappTests
+//
+//  Created by Shruthi Sathya on 4/16/25.
+//
+
+import Testing
+@testable import iosapp
+
+struct iosappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
