52749be1336e4c1ea0ca77d1e2da8020e97342f1
AshwinJ127 <108308607+AshwinJ127@users.noreply.github.com>
2025-12-11 18:13:42 -0800

    Fixed reset all to reset dosing input views

--- iosapp/LaunchResetManager.swift
+++ iosapp/LaunchResetManager.swift
@@ -73,22 +73,43 @@ enum LaunchResetManager {
     }
     
     static func resetAll(simulationData: SimulationData) {
-        // ---- Reset input defaults (these are the same keys you use in @AppStorage) ----
         let defaults: [String: Any] = [
             "t4Secretion": "100",
             "t3Secretion": "100",
             "t4Absorption": "88",
             "t3Absorption": "88",
-            "height": "170",
-            "weight": "70",
+            "heightCm": 170.0,
+            "weightKg": 70.0,
             "selectedHeightUnit": "cm",
             "selectedWeightUnit": "kg",
             "selectedGender": "FEMALE",
             "simulationDays": "5",
-            // Toggle ON by default:
             "isInitialConditionsOn": true,
-            // Keep Run 1 labeling as "Run 1" going forward if you use this flag
-            "hasRunRun1Once": true
+            "hasRunRun1Once": true,
+            
+            "T3OralDoseInput": "",
+            "T3OralDoseStart": "",
+            "T3OralDoseEnd": "",
+            "T3OralDoseInterval": "",
+            
+            "T4OralDoseInput": "",
+            "T4OralDoseStart": "",
+            "T4OralDoseEnd": "",
+            "T4OralDoseInterval": "",
+            
+            "T3IVDoseInput": "",
+            "T3IVDoseStart": "",
+            
+            "T4IVDoseInput": "",
+            "T4IVDoseStart": "",
+            
+            "T3InfusionDoseInput": "",
+            "T3InfusionDoseStart": "",
+            "T3InfusionDoseEnd": "",
+            
+            "T4InfusionDoseInput": "",
+            "T4InfusionDoseStart": "",
+            "T4InfusionDoseEnd": ""
         ]
 
         let ud = UserDefaults.standard
@@ -97,7 +118,6 @@ enum LaunchResetManager {
         }
         ud.synchronize()
 
-        // ---- Clear Run 2 & Run 3 state; keep Run 1 state ----
         simulationData.run1Result = nil
         simulationData.t4oralinputs.removeAll()
         simulationData.t3oralinputs.removeAll()
@@ -106,7 +126,6 @@ enum LaunchResetManager {
         simulationData.t4infusioninputs.removeAll()
         simulationData.t3infusioninputs.removeAll()
 
-        // Run 2 specific
         simulationData.run2T4oralinputs.removeAll()
         simulationData.run2T3oralinputs.removeAll()
         simulationData.run2T4ivinputs.removeAll()
@@ -115,8 +134,8 @@ enum LaunchResetManager {
         simulationData.run2T3infusioninputs.removeAll()
         simulationData.previousRun2Results.removeAll()
         simulationData.run2Result = nil
+        simulationData.currentRun2Result = nil
 
-        // Run 3 specific
         simulationData.run3T4oralinputs.removeAll()
         simulationData.run3T3oralinputs.removeAll()
         simulationData.run3T4ivinputs.removeAll()
@@ -124,9 +143,9 @@ enum LaunchResetManager {
         simulationData.run3T4infusioninputs.removeAll()
         simulationData.run3T3infusioninputs.removeAll()
         simulationData.previousRun3Results.removeAll()
-        simulationData.run3Result = nil // Clear current run3 result as well
+        simulationData.run3Result = nil
+        simulationData.currentRun3Result = nil
 
-        // Run 4 specific (NEW)
         simulationData.run4T4oralinputs.removeAll()
         simulationData.run4T3oralinputs.removeAll()
         simulationData.run4T4ivinputs.removeAll()
@@ -134,6 +153,7 @@ enum LaunchResetManager {
         simulationData.run4T4infusioninputs.removeAll()
         simulationData.run4T3infusioninputs.removeAll()
         simulationData.previousRun4Results.removeAll()
-        simulationData.run4Result = nil // Clear current run4 result
+        simulationData.run4Result = nil
+        simulationData.currentRun4Result = nil
     }
 }
